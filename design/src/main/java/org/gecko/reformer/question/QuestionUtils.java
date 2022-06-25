0249f58231d1e7385474cf0d2f771a95615e00ef
鲁志超 <416694798@qq.com>
2022-06-25 13:07:34 +0800

    设计模式-问题答题

---
+++ design/src/main/java/org/gecko/reformer/question/QuestionUtils.java
@@ -0,0 +1,41 @@
+package org.gecko.reformer.question;
+
+import java.util.*;
+
+/**
+ * @author LZC
+ * @version 1.0.0
+ * @date 2021/07/05
+ **/
+public class QuestionUtils {
+
+    /**
+     * 将问题选项乱序，并转成问题对象返回
+     *
+     * @param question 问题对象
+     * @return org.gecko.reformer.question.Question 问题选项乱序后的问题对象
+     * @throws
+     * @author LZC
+     * @date 2021-07-05
+     * @version 1.1.1
+     **/
+    public static Question random(Question question) {
+        String topic = question.getTopic();
+        Map<String, String> option = question.getOption();
+        String key = question.getKey();
+        Set<String> keySet = option.keySet();
+        ArrayList<String> keyList = new ArrayList<>(keySet);
+        Collections.shuffle(keyList);
+        HashMap<String, String> optionNew = new HashMap<>(option.size());
+        int idx = 0;
+        String keyNew = "";
+        for (String next : keySet) {
+            String randomKey = keyList.get(idx++);
+            if (key.equals(next)) {
+                keyNew = randomKey;
+            }
+            optionNew.put(randomKey, option.get(next));
+        }
+        return new Question(topic, optionNew, keyNew);
+    }
+}
---
+++ design/src/test/java/org/gecko/reformer/test/jzzms/DesignTest.java
@@ -0,0 +1,25 @@
+package org.gecko.reformer.test.jzzms;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * 模式测试
+ *
+ * @author LZC
+ * @version 1.0.0
+ * @date 2022-06-25
+ **/
+@Slf4j
+@SpringBootTest
+@RunWith(SpringRunner.class)
+public class DesignTest {
+
+    @Test
+    public void test() {
+
+    }
+}

01866bd7246480e16c61cff849cea890c71151ad
鲁志超 <416694798@qq.com>
2022-07-06 10:20:05 +0800

    job升级项目

---
+++ job-upgrade/src/main/java/org/gecko/reformer/constant/JobConstants.java
@@ -0,0 +1,13 @@
+package org.gecko.reformer.constant;
+
+/**
+ * TODO
+ *
+ * @author LZC
+ * @version 1.0.0
+ * @date 2022-07-06
+ **/
+public class JobConstants {
+
+    public static Integer JOB_EXCEPTION_INDEX = 0;
+}

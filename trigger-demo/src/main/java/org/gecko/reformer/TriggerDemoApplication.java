01dfe39105dcce70eaa1f21ed9986e89f07ea557
鲁志超 <416694798@qq.com>
2022-10-24 11:26:21 +0800

    测试自定义调度任务

--- trigger-demo/src/main/java/org/gecko/reformer/TriggerDemoApplication.java
+++ trigger-demo/src/main/java/org/gecko/reformer/TriggerDemoApplication.java
@@ -2,14 +2,12 @@ package org.gecko.reformer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * @author LZC
  * @version 1.1.2
  * @date 2022-09-08
  **/
-@EnableScheduling
 @SpringBootApplication
 public class TriggerDemoApplication {
     public static void main(String[] args) {

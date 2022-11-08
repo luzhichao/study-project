5a871dfbc13de422859621ee7ea9c78b5150a5fb
鲁志超 <416694798@qq.com>
2022-11-08 09:50:19 +0800

    sharding功能验证

--- shrading-jdbc/src/main/java/org/gecko/reformer/ShardingApplication.java
+++ shrading-jdbc/src/main/java/org/gecko/reformer/ShardingApplication.java
@@ -1,9 +1,9 @@
 package org.gecko.reformer;
 
+import org.gecko.reformer.annotation.ReformerApplication;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@ReformerApplication
 public class ShardingApplication {
     public static void main(String[] args) {
         SpringApplication.run(ShardingApplication.class, args);

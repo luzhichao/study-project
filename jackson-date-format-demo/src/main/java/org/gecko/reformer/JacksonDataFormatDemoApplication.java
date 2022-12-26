b8d4ab4c2f3d607a69d0491833369b8fc5f05e40
LuZhiChao <416694798@qq.com>
2022-12-26 10:11:43 +0800

    json时间格式统一

---
+++ jackson-date-format-demo/src/main/java/org/gecko/reformer/JacksonDataFormatDemoApplication.java
@@ -0,0 +1,16 @@
+package org.gecko.reformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author LZC
+ * @version 1.1.2
+ * @date 2022-09-08
+ **/
+@SpringBootApplication
+public class JacksonDataFormatDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonDataFormatDemoApplication.class, args);
+    }
+}
---
+++ jackson-date-format-demo/src/main/java/org/gecko/reformer/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package org.gecko.reformer.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+import org.gecko.reformer.entity.User;
+
+/**
+ * TODO
+ *
+ * @author LZC
+ * @version 1.1.2
+ * @date 2022-12-26
+ **/
+@Mapper
+public interface UserMapper extends BaseMapper<User> {
+}

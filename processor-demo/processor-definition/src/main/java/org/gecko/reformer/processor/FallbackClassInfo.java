2c81e07e76b49d19d88daad32126b7a63e619025
LuZhiChao <416694798@qq.com>
2023-01-06 08:52:48 +0800

    完善降级配置

--- processor-demo/processor-definition/src/main/java/org/gecko/reformer/processor/FallbackClassInfo.java
+++ processor-demo/processor-definition/src/main/java/org/gecko/reformer/processor/FallbackClassInfo.java
@@ -1,75 +1,33 @@
 package org.gecko.reformer.processor;
 
+import lombok.Data;
+
 import java.io.Serializable;
 import java.util.List;
 
 /**
- * TODO
+ * 降级类数据
  *
  * @author LZC
  * @version 1.1.2
  * @date 2023-01-03
  **/
-public class FallbackClassInfo implements Serializable {
+@Data
+class FallbackClassInfo implements Serializable {
     private static final long serialVersionUID = 8761278527492189800L;
-    /**
-     * 所在包名
-     */
+
+    /*** 所在包名 **/
     private String packageName;
-    /**
-     * 需要引入的包
-     */
+    /*** 需要引入的包 **/
     private List<String> imports;
-    /**
-     * 创建的类名称
-     */
+    /*** 类注解 **/
+    private List<String> annotations;
+    /*** 创建的类名称 **/
     private String className;
-    /**
-     * 泛型的名称
-     */
+    /*** 实现接口名称集合 **/
+    private List<String> implementsClassName;
+    /*** 泛型的名称 **/
     private String genericsName;
-    /**
-     * 方法列表
-     */
+    /*** 方法列表 **/
     private List<String> methods;
-
-    public String getPackageName() {
-        return packageName;
-    }
-
-    public void setPackageName(String packageName) {
-        this.packageName = packageName;
-    }
-
-    public List<String> getImports() {
-        return imports;
-    }
-
-    public void setImports(List<String> imports) {
-        this.imports = imports;
-    }
-
-    public String getClassName() {
-        return className;
-    }
-
-    public void setClassName(String className) {
-        this.className = className;
-    }
-
-    public String getGenericsName() {
-        return genericsName;
-    }
-
-    public void setGenericsName(String genericsName) {
-        this.genericsName = genericsName;
-    }
-
-    public List<String> getMethods() {
-        return methods;
-    }
-
-    public void setMethods(List<String> methods) {
-        this.methods = methods;
-    }
 }

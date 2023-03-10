7185c2168a101db70a58806596fe7db674334eda
LuZhiChao <416694798@qq.com>
2023-03-11 05:19:09 +0800

    flink

---
+++ flink-main/src/main/java/org/gecko/reformer/example/KafkaStreamWordCount.java
@@ -0,0 +1,40 @@
+package org.gecko.reformer.example;
+
+import org.apache.flink.api.common.typeinfo.Types;
+import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.streaming.api.datastream.DataStreamSource;
+import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.util.Collector;
+
+/**
+ * TODO
+ *
+ * @author LZC
+ * @version 1.1.2
+ * @date 2022-12-29
+ **/
+public class KafkaStreamWordCount {
+
+    public static void main(String[] args) throws Exception {
+
+        final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+
+        // nc -lk 7777
+        final DataStreamSource<String> lineDss = env.socketTextStream("localhost", 7777);
+
+        final SingleOutputStreamOperator<Tuple2<String, Integer>> sum = lineDss.flatMap((String line, Collector<Tuple2<String, Integer>> out) -> {
+            final String[] words = line.split(" ");
+            for (String word : words) {
+                out.collect(Tuple2.of(word, 1));
+            }
+        }).returns(Types.TUPLE(Types.STRING, Types.INT))
+                .keyBy(t -> t.f0)
+                .sum(1);
+
+        sum.print();
+
+        env.execute();
+    }
+
+}
---
+++ flink-spring/src/main/java/org/gecko/reformer/model/TestPgTableModel.java
@@ -0,0 +1,23 @@
+package org.gecko.reformer.model;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.util.Date;
+
+/**
+ * TODO
+ *
+ * @author LZC
+ * @version 1.1.2
+ * @date 2023-03-09
+ **/
+@Data
+public class TestPgTableModel implements Serializable {
+    private static final long serialVersionUID = 7184285960715646256L;
+
+    private String name;
+    private Date createTime;
+    private Double money;
+
+}

ebc93e09ff01ab00d78464ce727c3760af8aaa6d
LuZhiChao <416694798@qq.com>
2023-03-23 15:55:06 +0800

    redis

---
+++ gecko-spring-boot-demo/src/main/java/org/gecko/reformer/redis/CacheTestDTO2.java
@@ -0,0 +1,24 @@
+package org.gecko.reformer.redis;
+
+import lombok.Data;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+/**
+ * TODO
+ *
+ * @author LZC
+ * @version 1.2.0
+ * @date 2023-03-23
+ **/
+@Data
+public class CacheTestDTO2 implements Serializable {
+
+    private String name;
+    private LocalDate localDate;
+    private LocalTime localTime;
+    private LocalDateTime localDateTime;
+}

4a7957b72630fb04219a528d9e8c757916f71069
LuZhiChao <416694798@qq.com>
2023-04-24 10:16:32 +0800

    验证日志打印

--- gecko-service-demo/src/main/java/org/gecko/reformer/log/controller/TestLogController.java
+++ gecko-service-demo/src/main/java/org/gecko/reformer/log/controller/TestLogController.java
@@ -1,6 +1,5 @@
 package org.gecko.reformer.log.controller;
 
-import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.gecko.reformer.annotation.SysLog;
 import org.gecko.reformer.annotation.WebController;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
  * @version 1.1.2
  * @date 2023-01-05
  **/
-@Api(tags = "LogFeign")
+//@Api(tags = "LogFeign")
 @WebController
 @RequestMapping("/api/test-log-controller/")
 public class TestLogController {
